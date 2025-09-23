ea014448521ec6d98ea6701ac049d47f06a3ec7b
fidus-Achates <110263578+fidus-Achates@users.noreply.github.com>
2025-09-23 22:07:11 +0200

    styling intro page, add favicon

--- js/navigation.js
+++ js/navigation.js
@@ -2,12 +2,14 @@ const latinoMusic = new Audio("./audio/happy-acoustic-pop-in-latin-feel-224317-[
 // const latinoDance = new Audio("./audio/latina-noche-dance-latin-house-background-mexican-reggaeton-music-160302-[AudioTrimmer.com](3).mp3");
 latinoMusic.loop = true;
 // latinoDance.loop = true;
+let musicStarted = false;
 
 function playMusic(sound) {
   const soundIcon = document.getElementById("soundIcon");
-  if(sound.paused) {
+  if(!musicStarted) {
     sound.play();
     sound.muted = false;
+    musicStarted = true;
   } else {
     sound.muted = !sound.muted;
   }
@@ -37,38 +39,46 @@ function home() {
 function getStory() {
   return `
     <div class="content">
-      <p>You are "Pepe el peligroso", the fearless chicken fighter.</p>
-      <p>Your mission: collect coins and all five super-spicy Salsa bottles.</p> 
-      <p>Be quick, because more and more chickens are attacking you!</p>
-      <p>Avoid contact so you don't get hurt.</p>
-      <p>You can hop over the enemies or crush them by jumping on them (this gives you extra energy points).</p>
-      <p>Find the dangerous endboss and pelt him with all your bottles until he burns up.</p>
-      <div class="buttons-div"><button class="title-button" onclick="displayContent('controls')">How to move</button></div>
+      <div>
+        <p>You are Pepe el peligroso, the fearless chicken fighter.</p>
+        <p>Your mission: collect coins and all five super-spicy Salsa bottles.</p> 
+        <p>Be quick, because more and more chickens are attacking you!</p>
+        <p>Avoid contact so you don't get hurt.</p>
+        <p>You can hop over the enemies or crush them by jumping on them</p>
+        <p>(this gives you extra energy points).</p>
+        <p>Find the endboss and pelt him with all your bottles until he burns up.</p>
+      </div>
+      <div class="buttons-div"><button class="title-button medium" onclick="displayContent('controls')">How to move</button></div>
     </div>
   `;
 }
 
 function getControlButtons() {
   return `
-    <div class="content">
-      <p>Left</p>
-      <p>Right</p> 
-      <p>Jump</p>
-      <p>Throw Bottle</p>
-      <div class="buttons-div"><button class="title-button">Start Game</button> <img src="./assets/home.png" class="title-button" onclick="home()"></div>
+    <div class="content middle">
+      <table class="moves">
+        <tr><td><img class="key" src="./assets/arrowL.png"></td> <td>Move to left</td></tr> 
+        <tr><td><img class="key" src="./assets/arrowR.png"></td> <td>Move to right</td></tr>
+        <tr><td><span class="key">space</span></td> <td>Jump</td></tr>
+        <tr><td><img class="key" src="./assets/arrowUp.png"></td> <td>Throw Bottle</td></tr>
+      </table>
+      <div class="buttons-div narrow-gap"><button class="title-button medium">Start Game</button> <img src="./assets/home.png" class="title-button medium icon home" onclick="home()"></div>
     </div>
   `;
 }
 
 function getCredits() {
   return `
-    <div class="content">
-      <p>Game graphics: copyright Developerakademie</p>
-      <p>Background image: ###</p> 
-      <p>Background music: ###</p>
-      <p>Sounds: ###</p>
-      <p>Code: Claudia Wick 2025</p>
-      <div class="buttons-div"><button class="title-button">Start Game</button> <img src="./assets/home.png" class="title-button" onclick="home()"></div>
+    <div class="content middle">
+      <table class="credits">
+        <tr><td>Game graphics:</td> <td>Developerakademie</td></tr>
+        <tr><td>Background image:</td> <td><a href="https://www.freepik.com/free-ai-image/desert-cacti-nature_149766888.htm#fromView=image_search_similar&page=1&position=41&uuid=9f9e9985-2387-437d-a693-eaa4a664422e&query=Gaming+with+western+elements" target="_blank">Freep!k</a></td></tr> 
+        <tr><td>Background music:</td> <td><a href="https://pixabay.com/de/music/optimistisch-happy-acoustic-pop-in-latin-feel-224317/" target="_blank">Sonican (Pixabay)</a></td></tr>
+        <tr><td>Sounds:</td> <td>Pixabay</td></tr>
+        <tr><td>Font:</td> <td><a href="https://fontmeme.com/fonts/zabars-font/" target="_blank">Zabars</a></td></tr>
+        <tr><td>Code, animated gif:</td> <td>Claudia Wick 2025</td></tr>
+      </table>
+      <div class="buttons-div narrow-gap"><button class="title-button medium">Start Game</button> <img src="./assets/home.png" class="title-button medium icon home" onclick="home()"></div>
     </div>
   `;
 }
